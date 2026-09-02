91c7b6c7f7324565ea4dc12c1a15b17a5282a65d
ItamRenffeh <matiasheffner34@gmail.com>
2026-09-02 18:45:53 -0300

    Create Test.py

---
+++ Capitulo-1/Test.py
@@ -0,0 +1,18 @@
+import string
+
+
+contador = 0
+while contador < 5:
+ print("Hola mundo")
+ contador += 1
+ print(2+2+contador)
+ print(contador**2)
+ print("Hola"*contador)
+ Numero= int(input("Ingrese un numero: "))
+ print(str(Numero) + " es el numero ingresado")
+ nombre = input("Ingrese su nombre: ")
+ print("Hola " + nombre + " bienvenido al programa")
+ print("Su nombre tiene" + str(len(nombre)) + " letras")
+ 
+ 
+ 
